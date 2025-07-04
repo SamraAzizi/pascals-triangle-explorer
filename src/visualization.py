53db5f433bbeaa8f0f4b4efb046632f107803d41
SamraAzizi <samarraazizi@gmail.com>
2025-07-04 15:43:09 +0500

    pascals triangle

--- src/visualization.py
+++ src/visualization.py
@@ -14,3 +14,16 @@ def display_triangle(triangle):
     max_width = len(' '.join(map(str, triangle[-1])))
     for row in triangle:
         print(' '.join(map(str, row)).center(max_width))
+
+def display_colored_triangle(triangle):
+    """
+    Print Pascal's Triangle with even/odd numbers colored differently
+    """
+    max_width = len(' '.join(['X' for _ in triangle[-1]]))  # Account for color codes
+    
+    for row in triangle:
+        colored_row = []
+        for num in row:
+            if num % 2 == 0:
+                colored_num = f"{COLORS['blue']}{num}{COLORS['reset']}"
+            else:
