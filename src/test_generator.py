cdd740d6298a0b8386b03a5aac67150e57179c4a
SamraAzizi <samarraazizi@gmail.com>
2025-07-04 15:44:27 +0500

    pascals triangle

---
+++ src/test_generator.py
@@ -0,0 +1,13 @@
+import unittest
+from src.triangle_generator import generate_pascals_triangle
+
+class TestTriangleGenerator(unittest.TestCase):
+    def test_triangle_generation(self):
+        expected = [
+            [1],
+            [1, 1],
+            [1, 2, 1],
+            [1, 3, 3, 1],
+            [1, 4, 6, 4, 1]
+        ]
+        self.assertEqual(generate_pascals_triangle(5), expected)
