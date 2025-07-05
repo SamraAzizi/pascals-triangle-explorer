9efb3a08130884c7b33284d81f9e74f426726062
SamraAzizi <samarraazizi@gmail.com>
2025-07-05 21:09:37 +0500

    pascals triangle

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -17,3 +17,16 @@ class TestTriangleGenerator(unittest.TestCase):
 
     def test_empty_triangle(self):
         self.assertEqual(generate_pascals_triangle(0), [])
+
+    def test_large_triangle(self):
+        triangle = generate_pascals_triangle(10)
+        self.assertEqual(len(triangle), 10)
+        self.assertEqual(triangle[-1], [1, 9, 36, 84, 126, 126, 84, 36, 9, 1])
+
+    def test_row_symmetry(self):
+        triangle = generate_pascals_triangle(7)
+        for row in triangle:
+            self.assertEqual(row, row[::-1])  # Each row should be symmetric
+
+if __name__ == '__main__':
+    unittest.main()
